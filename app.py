822d2655e7c929d938670cdfb2e454193f757ce8
Sibasish018 <sibasish.official018@gmail.com>
2026-03-20 09:44:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -257,4 +257,4 @@ def logout():
 import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
